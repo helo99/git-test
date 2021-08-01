a8bb2a5cf9bd4fd02b9a036e15189b0fdc104c3f
helo99 <cn_swd@163.com>
2021-08-01 11:18:17 +0800

    idea first commit!

---
+++ src/main/java/com/swd/HelloGit.java
@@ -0,0 +1,7 @@
+package com.swd;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
---
+++ src/main/java/com/swd/HelloGit2.java
@@ -0,0 +1,4 @@
+package com.swd;
+
+public class HelloGit2 {
+}
